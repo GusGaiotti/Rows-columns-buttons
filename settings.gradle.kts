f33e8ca61f6c3dd5f80a434836a529342b2b54d4
Gustavo Gaiotti <gustavogaiotti2014@gmail.com>
2023-08-13 23:42:06 -0300

    Studying rows, columns and buttons

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Column Row"
+include(":app")
+ 
